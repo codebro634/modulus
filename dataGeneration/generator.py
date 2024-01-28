90ecd37a4b21abf6286813997b58e0f8960b8418
Robin Schmöcker <schmoecker@wilson.ifam.uni-hannover.de>
2024-01-28 17:12:58 +0100

    added basic fenics based data generation for cylinder flow

---
+++ dataGeneration/generator.py
@@ -0,0 +1,198 @@
+from fenics import *
+from mshr import *
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import os
+import imageio
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--p", action="store_true", help="If set, save gif of the simulation.")
+parser.add_argument("--v", action="store_true", help="Activate verbosity.")
+parser.add_argument('--dir', default="navier_stokes_cylinder", help='Path to where results are stored')
+args = parser.parse_args()
+
+results_dir = args.dir
+os.makedirs(results_dir,exist_ok=True)
+
+#Plotting params
+num_imgs = 100
+
+if args.p:
+    plot_path = os.path.join(results_dir,"animation")
+    if not os.path.exists(plot_path):
+        os.makedirs(plot_path,exist_ok=True)
+
+
+start = time.time()
+
+T = 5.0            # final time
+num_steps = 5000   # number of time steps
+dt = T / num_steps # time step size
+mu = 0.001         # dynamic viscosity
+rho = 1            # density
+
+# Create mesh
+channel = Rectangle(Point(0, 0), Point(2.2, 0.41))
+cylinder = Circle(Point(0.2, 0.2), 0.05)
+domain = channel - cylinder
+mesh = generate_mesh(domain, 64)
+if args.v:
+    print(f"{mesh.num_vertices()} vertices in mesh.")
+
+# Define function spaces
+V = VectorFunctionSpace(mesh, 'P', 2)
+Q = FunctionSpace(mesh, 'P', 1)
+
+# Define boundaries
+inflow   = 'near(x[0], 0)'
+outflow  = 'near(x[0], 2.2)'
+walls    = 'near(x[1], 0) || near(x[1], 0.41)'
+cylinder = 'on_boundary && x[0]>0.1 && x[0]<0.3 && x[1]>0.1 && x[1]<0.3'
+
+# Define inflow profile
+inflow_profile = ('4.0*1.5*x[1]*(0.41 - x[1]) / pow(0.41, 2)', '0')
+
+# Define boundary conditions
+bcu_inflow = DirichletBC(V, Expression(inflow_profile, degree=2), inflow)
+bcu_walls = DirichletBC(V, Constant((0, 0)), walls)
+bcu_cylinder = DirichletBC(V, Constant((0, 0)), cylinder)
+bcp_outflow = DirichletBC(Q, Constant(0), outflow)
+bcu = [bcu_inflow, bcu_walls, bcu_cylinder]
+bcp = [bcp_outflow]
+
+# Define trial and test functions
+u = TrialFunction(V)
+v = TestFunction(V)
+p = TrialFunction(Q)
+q = TestFunction(Q)
+
+# Define functions for solutions at previous and current time steps
+u_n = Function(V)
+u_  = Function(V)
+p_n = Function(Q)
+p_  = Function(Q)
+
+# Define expressions used in variational forms
+U  = 0.5*(u_n + u)
+n  = FacetNormal(mesh)
+f  = Constant((0, 0))
+k  = Constant(dt)
+mu = Constant(mu)
+rho = Constant(rho)
+
+# Define symmetric gradient
+def epsilon(u):
+    return sym(nabla_grad(u))
+
+# Define stress tensor
+def sigma(u, p):
+    return 2*mu*epsilon(u) - p*Identity(len(u))
+
+# Define variational problem for step 1
+F1 = rho*dot((u - u_n) / k, v)*dx + rho*dot(dot(u_n, nabla_grad(u_n)), v)*dx + inner(sigma(U, p_n), epsilon(v))*dx + dot(p_n*n, v)*ds - dot(mu*nabla_grad(U)*n, v)*ds - dot(f, v)*dx
+a1 = lhs(F1)
+L1 = rhs(F1)
+
+# Define variational problem for step 2
+a2 = dot(nabla_grad(p), nabla_grad(q))*dx
+L2 = dot(nabla_grad(p_n), nabla_grad(q))*dx - (1/k)*div(u_)*q*dx
+
+# Define variational problem for step 3
+a3 = dot(u, v)*dx
+L3 = dot(u_, v)*dx - k*dot(nabla_grad(p_ - p_n), v)*dx
+
+# Assemble matrices
+A1 = assemble(a1)
+A2 = assemble(a2)
+A3 = assemble(a3)
+
+# Apply boundary conditions to matrices
+[bc.apply(A1) for bc in bcu]
+[bc.apply(A2) for bc in bcp]
+
+# Create XDMF files for visualization output
+xdmffile_u = XDMFFile(results_dir+'/velocity.xdmf')
+xdmffile_p = XDMFFile(results_dir+'/pressure.xdmf')
+
+# Create time series (for use in reaction_system.py)
+timeseries_u = TimeSeries(results_dir+'/velocity_series')
+timeseries_p = TimeSeries(results_dir+'/pressure_series')
+
+# Save mesh to file (for use in reaction_system.py)
+File(results_dir+'/cylinder.xml.gz') << mesh
+
+# Time-stepping
+t = 0
+image_v_locs,image_p_locs = [],[]
+
+for n in range(num_steps): #num_steps
+
+    # Update current time
+    t += dt
+
+    # Step 1: Tentative velocity step
+    b1 = assemble(L1)
+    [bc.apply(b1) for bc in bcu]
+    solve(A1, u_.vector(), b1, 'bicgstab', 'hypre_amg')
+
+    # Step 2: Pressure correction step
+    b2 = assemble(L2)
+    [bc.apply(b2) for bc in bcp]
+    solve(A2, p_.vector(), b2, 'bicgstab', 'hypre_amg')
+
+    # Step 3: Velocity correction step
+    b3 = assemble(L3)
+    solve(A3, u_.vector(), b3, 'cg', 'sor')
+
+    # Plot solution
+    if args.p and n % (num_steps // num_imgs) == 0:
+        title=f"velocity{n}"
+        plot(u_, title=title)
+        plt.savefig(os.path.join(plot_path,title+".png"))
+        image_v_locs.append(os.path.join(plot_path,title+".png"))
+
+        title = f"pressure{n}"
+        plot(p_,title=title)
+        plt.savefig(os.path.join(plot_path,title+".png"))
+        image_p_locs.append(os.path.join(plot_path,title+".png"))
+
+    # Save solution to file (XDMF/HDF5)
+    xdmffile_u.write(u_, t)
+    xdmffile_p.write(p_, t)
+
+    # Save nodal values to file
+    timeseries_u.store(u_.vector(), t)
+    timeseries_p.store(p_.vector(), t)
+
+    # Update previous solution
+    u_n.assign(u_)
+    p_n.assign(p_)
+
+    # Print progress
+    if args.v:
+        print(f"Progress {t/T}")
+    
+if args.v:
+    print(f"Duration: {time.time() -  start}")
+
+#Create animation
+if args.p:
+    duration = (num_steps // num_imgs) * dt / 2 #Divided by 2 to have a bit of slow-motion
+    with imageio.get_writer(os.path.join(plot_path,'velocity.gif'), mode='I', duration=duration) as writer:
+        for image in image_v_locs:
+            img = imageio.imread(image)
+            writer.append_data(img)
+
+    for image in image_v_locs:
+        os.remove(image)
+
+    with imageio.get_writer(os.path.join(plot_path,'pressure.gif'), mode='I', duration=duration) as writer:
+        for image in image_p_locs:
+            img = imageio.imread(image)
+            writer.append_data(img)
+
+    for image in image_p_locs:
+        os.remove(image)
